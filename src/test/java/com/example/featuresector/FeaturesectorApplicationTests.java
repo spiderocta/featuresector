cb6328545408fcc6f4a41352f7afb066c692ee23
spiderocta <cryptofer@tuta.io>
2022-10-28 13:40:50 +0200

    initial commit

---
+++ src/test/java/com/example/featuresector/FeaturesectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.featuresector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturesectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
