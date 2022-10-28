a42090656780cf7b5dc3d80a7653073318f1b0e1
spiderocta <cryptofer@tuta.io>
2022-10-28 15:33:26 +0200

    comment entity with realtion

---
+++ src/main/java/com/featuresector/domain/Comment.java
@@ -0,0 +1,30 @@
+package com.featuresector.domain;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class Comment {
+	
+	private CommentId pk;
+	private String text;
+	
+	@EmbeddedId
+	public CommentId getPk() {
+		return pk;
+	}
+	public void setPk(CommentId pk) {
+		this.pk = pk;
+	}
+	
+	@Column(length = 5000)
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
