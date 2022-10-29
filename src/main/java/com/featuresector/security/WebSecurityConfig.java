3cbbf364a812d854ee92a404e81d42511ddc7d96
spiderocta <cryptofer@tuta.io>
2022-10-29 21:29:05 +0200

    password encoder

--- src/main/java/com/featuresector/security/WebSecurityConfig.java
+++ src/main/java/com/featuresector/security/WebSecurityConfig.java
@@ -1,22 +1,36 @@
 package com.featuresector.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("user@featuresector.com")
-		.password("password")
-		.roles("USER");
-	}
+	
+	  @Autowired
+	  private UserDetailsService userDetailsService;
+
+	  @Bean
+	  public PasswordEncoder getPasswordEncoder() {
+	    return new BCryptPasswordEncoder();
+	  }
+	  
+	  @Override
+	  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	    auth
+	      .userDetailsService(userDetailsService)
+	      .passwordEncoder(getPasswordEncoder());
+	        
+	  }
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
