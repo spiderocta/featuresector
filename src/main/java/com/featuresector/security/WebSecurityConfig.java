b659bbd316908191dcf70e17418b42de40aac893
spiderocta <cryptofer@tuta.io>
2022-10-29 21:41:53 +0200

    dashboard page and redirects

--- src/main/java/com/featuresector/security/WebSecurityConfig.java
+++ src/main/java/com/featuresector/security/WebSecurityConfig.java
@@ -1,13 +1,11 @@
 package com.featuresector.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,21 +14,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	  @Autowired
-	  private UserDetailsService userDetailsService;
-
-	  @Bean
+	   @Bean
 	  public PasswordEncoder getPasswordEncoder() {
 	    return new BCryptPasswordEncoder();
 	  }
-	  
-	  @Override
-	  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	    auth
-	      .userDetailsService(userDetailsService)
-	      .passwordEncoder(getPasswordEncoder());
-	        
-	  }
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.passwordEncoder(getPasswordEncoder())
+		.withUser("user@featuresector.com")
+		.password(getPasswordEncoder().encode("password"))
+		.roles("USER");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
